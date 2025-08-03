1e64c83ede3776026b8cac211fcd49dded796016
x_Dragon_x <dragonek.gram@gmail.com>
2025-08-03 09:27:56 +0200

    Update ApiResponse.java
    Usuniecie tego gowna nie potrzebnego

--- src/main/java/pl/serweryminecraft24/sm24VoterJava/api/ApiResponse.java
+++ src/main/java/pl/serweryminecraft24/sm24VoterJava/api/ApiResponse.java
@@ -1,47 +1,7 @@
 package pl.serweryminecraft24.sm24VoterJava.api;
 
-import java.util.Objects;
-
-
-public final class ApiResponse {
-
-    private final boolean success;
-    private final String message;
-
-
-    public ApiResponse(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ApiResponse that = (ApiResponse) o;
-        return success == that.success && Objects.equals(message, that.message);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(success, message);
-    }
-
-    @Override
-    public String toString() {
-        return "ApiResponse{" +
-                "success=" + success +
-                ", message='" + message + '\'' +
-                '}';
+public record ApiResponse(boolean success, String message) {
+    public ApiResponse {
+        message = message != null ? message : "";
     }
 }
